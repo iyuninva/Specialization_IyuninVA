96adc0853eb67502a296eb24057dcfc6153413f1
IyuninVA <IyuninVA@bk.ru>
2023-02-16 22:46:11 +0400

    Program.cs_v.1.0

--- Project/Program.cs
+++ Project/Program.cs
@@ -4,29 +4,53 @@ string[] ArrayCompletion(string[] Array)
 {
     for (int i = 0; i < Array.Length; i++)
     {
+        Console.Write($"Array[{i + 1}]: ");
         Array[i] = Console.ReadLine()!;
-        if (Array[i] == "")
+        do
         {
-            Array[i] = Console.ReadLine()!;
+            if (Array[i] == "")
+            {
+                Console.Write($"Array[{i + 1}]: ");
+                Array[i] = Console.ReadLine()!;
+            }
         }
+        while (Array[i] == "");
     }
+    Console.WriteLine();
+    Console.Write("Вы заполнили массив следующими данными: ");
     Console.WriteLine(string.Join(", ", Array));
-    ArrayTransformation(Array);
+    Console.Write("Желайте изменить введенные данные?: (yes/no) ");
+    if (Console.ReadLine()! == "yes")
+    {
+        ArrayCompletion(Array);
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.Write("Нажмите Enter для продолжения: ");
+        Console.ReadKey();
+        ArrayTransformation(Array);
+    }
     return Array;
 }
 
 // Метод преоразования массива
 void ArrayTransformation(string[] Array)
 {
-    string[] ArrayNow = new string[new Random().Next(1, 3)];
+    string[] ArrayNow = new string[new Random().Next(1, 4)];
     for (int j = 0; j < ArrayNow.Length; j++)
     {
-        ArrayNow[j] = Array[new Random().Next(0, 3)];
+        ArrayNow[j] = Array[new Random().Next(0, 5)];
     }
+    Console.WriteLine();
+    Console.Write("Новый массив строк: ");
     Console.WriteLine(string.Join(", ", ArrayNow));
 }
 
 // Вход в программу
 Console.WriteLine("Программа формирования нового массива строк");
-string[] Array = new string[3];
+Console.Write("Нажмите Enter для запуска программы: ");
+Console.ReadKey();
+Console.WriteLine();
+string[] Array = new string[5];
 ArrayCompletion(Array);
